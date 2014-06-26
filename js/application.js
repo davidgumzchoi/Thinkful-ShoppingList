009ca89c9f017942d06c98f4a87e825b213945a6
GumZ <davidgumzchoi@gmail.com>
2014-06-26 15:10:14 -0700

    fully functioning app

--- js/application.js
+++ js/application.js
@@ -1,43 +1,88 @@
+// Global for initial guess
 var answer = Math.floor((Math.random() * 100) + 1);
-var $help = $('.help');
-var $form = $('form');
-var $input = $('input');
-var $submitButton = $('.submit');
-var $response = $('.response');
-var $cheat = $('.cheat');
-var $reset = $('.reset');
-
-$help.hide();
-
-$form.submit(function(e) {
-  if (parseInt($input.val()) === answer) {
-  	e.preventDefault();
-  	$response.text('correct!');
-  } else {
-  	e.preventDefault();
-  	$response.text('incorrect...');
-  }
+// Global for previous guess
+var previousGuess;
+// Global for number of guesses
+var numberOfGuesses = 1;
+// Validate as normal integer
+function isNormalInteger(str) {
+    return (/^[1-9]\d*$/).test(str);
+}
+// Checks to see if the guess is within the parameters given
+function validGuess(guess) {
+    return isNormalInteger(guess) && +guess <= 100 && +guess >= 1
+}
+// Compare the guess to the number and previous guess. Place feedback on the page for the player.
+function compareGuess(event) {
+    event.preventDefault();
+    // Grab the guess from the text input field
+    var guess = $('#guess').val();
+    if (validGuess(guess)) {
+        // Turn off any error messages
+        $('.help').hide();
+        // Increment number of guesses
+        numberOfGuesses++;
+		if (previousGuess) {
+            // Find distances of the current and previous guesses from the actual number
+            var previousDistance = Math.abs(answer - previousGuess);
+            var currentDistance = Math.abs(answer - guess);
+            // Feedback for guess versus previous guess comparison
+            if (guess === previousGuess) {
+                $('.almost').text("Same guess!");
+            } else if (currentDistance < previousDistance){
+                $('.almost').text("Getting warmer...");
+            } else if (currentDistance > previousDistance) {
+                $('.almost').text("Getting colder...");
+            } else {
+                $('.almost').text("Same distance...");
+            }
+        }
+        // Set new previous guess
+        previousGuess = guess;
+        // Convert guess value to an integer for comparison
+        guess = parseInt(guess, 10);
+        // Feedback for a correct guess. Show the reset button to start a new game.
+        if (guess === answer) {
+            $('.response').text('The number was ' + answer + '.');
+            $('.almost').text('');
+            if (numberOfGuesses === 1) {
+            	$('.almost').text('You got it on the first try!');
+            } else {
+            	$('.almost').text('You got it in ' + numberOfGuesses + ' guesses.');
+            }
+        // Feedback for a low guess
+        } else if (answer > guess) {
+            $('.response').text('Higher than ' + guess + '!');
+        // Feedback for a high guess
+        } else {
+            $('.response').text('Lower than ' + guess + '!');
+        }
+        // Blank out the guess input field return focus
+        $('#guess').val('').focus();
+    } else {
+        // Give error for invalid guess. Blank out the guess field and return focus.
+        $('.help').text('Please enter valid integer between 1 and 100').show();
+        $('#guess').val('').focus();
+    }
+}
+// Bind form submission to the compareGuess function
+$('form').submit(compareGuess);
+// Bind enter key to the compareGuess function for browsers that don't always interpret an enter press as a form submission.
+$('#guess').keypress(function(event) {
+    if (event.which == 13) {
+        compareGuess();
+    }
 });
-
-$cheat.click(function(e) {
+// Reveals answer to user
+$('.cheat').click(function(e) {
 	e.preventDefault();
 	alert(answer);
 });
-
-$reset.click(function(e) {
+// Assigns new hidden answer and empty's input
+$('.reset').click(function(e) {
 	e.preventDefault();
 	answer = Math.floor((Math.random() * 100) + 1);
+	$('.correct').text('');
+	$('.response').text('');
+	$('.input').val('').focus();
 });
-
-// validate input
-   // spaces, blank guess, letters
-// no repeating
-// getting hotter or colder
-   // too high
-   // too low
-// track the user's previous guess to let them if they getting hotter or colder
-// animations using jQuery
-
-// If you get stuck... 
-// http://philiplang.github.io/HuckleBuckle/index.html
-// http://raddevon.github.io/huckle-buckle-beanstalk/
